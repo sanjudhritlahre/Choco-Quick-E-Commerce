aefb9e094a3dc140f5381bbb15028c9b9e67b642
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-30 01:52:48 +0530

    Get All Delivery Persons Endpoint

--- src/app/api/delivery-persons/route.ts
+++ src/app/api/delivery-persons/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPersons } from "@/lib/db/schema";
+import { deliveryPersons, warehouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validators/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
     const requestData = await request.json();
@@ -23,6 +24,31 @@ export async function POST(request: Request) {
                 error: err instanceof Error ? err.message : String(err),
             },
             { status: 500 }
+        );
+    }
+}
+
+export async function GET() {
+    try {
+        const allDeliveryPersons = await db
+            .select({
+                id: deliveryPersons.id,
+                name: deliveryPersons.name,
+                phone: deliveryPersons.phone,
+                warehouse: warehouses.name,
+            })
+            .from(deliveryPersons)
+            .leftJoin(warehouses, eq(deliveryPersons.warehouseId, warehouses.id))
+            .orderBy(desc(deliveryPersons.id));
+
+        return Response.json(allDeliveryPersons);
+    } catch (err) {
+        return Response.json(
+            {
+                message: 'Failed to fetch delivery persons',
+                error: err instanceof Error ? err.message : String(err),
+            },
+            { status: 500 },
         );
     }
 }
