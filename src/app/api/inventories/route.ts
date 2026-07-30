1fffeb04b51253344fb4480523da21087ee67cde
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-30 13:45:35 +0530

    Get All Inventories

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schema";
+import { inventories, products, warehouses } from "@/lib/db/schema";
 import { inventorySchema } from "@/lib/validators/inventorySchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
     const requestData = await request.json();
@@ -25,6 +26,32 @@ export async function POST(request: Request) {
                 message: 'Failed to store the inventory into the database',
                 error: err instanceof Error ? err.message : String(err),
             },
+            { status: 500 },
+        );
+    }
+}
+
+export async function GET() {
+    try {
+        const allInventories = await db
+            .select({
+                id: inventories.id,
+                sku: inventories.sku,
+                warehouse: warehouses.name,
+                product: products.name,
+            })
+            .from(inventories)
+            .leftJoin(warehouses, eq(inventories.warehouseId, warehouses.id))
+            .leftJoin(products, eq(inventories.productId, products.id))
+            .orderBy(desc(inventories.id));
+
+        return Response.json(allInventories);
+    } catch (err) {
+        return Response.json(
+            { 
+                message: 'Failed to fetch inventories', 
+                error: err instanceof Error ? err.message : String(err), 
+            }, 
             { status: 500 },
         );
     }
